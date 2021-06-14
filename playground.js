4739915c22de976818e7adeb4cf5e450a3f38a9d
J Duff <jaduff20@gmail.com>
2021-06-14 18:32:30 -0400

    refresher on promises and async in JS

--- playground.js
+++ playground.js
@@ -1,72 +1,22 @@
-// const myName = "Jayson";
-
-// console.log(myName);
-
-// let age = 100;
-// const hasFriends = false;
-// age = 30;
-
-// const summarizeUser = (userName, userAge, userFriends) => {
-//   return (
-//     "Name is " +
-//     userName +
-//     ", age is " +
-//     userAge +
-//     " and the user has friends? " +
-//     userFriends
-//   );
-// };
-
-// // const add = (a, b) => a + b;
-// // const addOne = (a) => a + 1;
-// const addRandom = () => 1 + 2;
-
-// // console.log(addOne(2));
-// console.log(addRandom());
-
-// console.log(summarizeUser(myName, age, hasFriends));
-
-//METHOD CALLED OFF OBJECT
-const person = {
-  name: "Jayson",
-  age: "30",
-  greet() {
-    console.log("Hi, I am " + this.name);
-  },
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
 };
 
-const printName = ({ name }) => {
-  console.log(name)
-}
-
-printName(person);
-
-const { name, age } = person;
-console.log(name, age);
-
-const hobbies = ['Sports', 'Cooking'];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
-
-
-
-// person.greet();
-
-// const copiedPerson = { ...person };
-// console.log(copiedPerson);
-
-// const hobbies = ["Sports", "Cooking"];
-// // for (let hobby of hobbies) {
-// //   console.log(hobby);
-// // }
-// // console.log(hobbies.map((hobby) => "Hobby: " + hobby));
-// // console.log(hobbies);
-// const copiedArray = [...hobbies];
-// console.log(copiedArray);
-
-// const toArray = (...args) => {
-//   return args;
-// };
-
-// console.log(toArray(1, 2, 3, 4));
-
+setTimeout(() => {
+  console.log("Timer is done!");
+  fetchData().then((text) => {
+    fetchData()
+      .then((text) => {
+        console.log(text);
+        return fetchData();
+      })
+      .then((text2) => {
+        console.log(text2);
+      });
+  });
+}, 2000);
