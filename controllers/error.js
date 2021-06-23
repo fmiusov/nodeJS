9b8cf375a382b806b712eb86e9a9da53f1accadc
J Duff <jaduff20@gmail.com>
2021-06-23 19:40:39 -0400

    edit and delete functionality added for products

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,5 @@
 exports.get404 = (req, res, next) => {
   res
     .status(404)
-    .render('page-not-found', { pageTitle: '404', path: '/404' });
+    .render('page-not-found', { pageTitle: '404', path: '404' });
 };
