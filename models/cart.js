6d0823f00941ab7a1fc5c92621bf6516b36d2402
Jayson Duff <jayson.duff@gsgus.com>
2021-06-24 17:33:37 -0400

    debugging cart model

--- models/cart.js
+++ models/cart.js
@@ -15,26 +15,26 @@ module.exports = class Cart {
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-    });
-    // Analyze the cart => Find existing product
-    const existingProductIndex = cart.products.findIndex(
-      (prod) => prod.id == id
-    );
-    const existingProduct = cart.products[existingProductIndex];
-    let updatedProduct;
-    // Add new product/ increase quantity
-    if (existingProduct) {
-      updatedProduct = { ...existingProduct };
-      updatedProduct.qty = updatedProduct.qty + 1;
-      cart.products = [...cart.products];
-      cart.products[existingProductIndex] = updatedProduct;
-    } else {
-      updatedProduct = { id: id, qty: 1 };
-      cart.products = [...cart.products, updatedProduct];
-    }
-    cart.totalPrice = cart.totalPrice + productPrice;
-    fs.writeFile(p, JSON.stringify(cart), (err) => {
-      console.log(err);
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id == id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
     });
   }
 };
