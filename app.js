46b693f4a3d2094f7c69468db0c2354f905981fd
Jayson Duff <jayson.duff@gsgus.com>
2021-06-29 16:15:22 -0400

    connected app to mysql db

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,6 +12,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
